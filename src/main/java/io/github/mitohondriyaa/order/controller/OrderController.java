a523a8f31239b3418e4cf6082ed0c4c1b759fce6
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-05 21:28:32 +0300

    Add MySQL migration through Flyway

--- src/main/java/io/github/mitohondriyaa/order/controller/OrderController.java
+++ src/main/java/io/github/mitohondriyaa/order/controller/OrderController.java
@@ -1,4 +1,21 @@
 package io.github.mitohondriyaa.order.controller;
 
+import io.github.mitohondriyaa.order.dto.OrderRequest;
+import io.github.mitohondriyaa.order.dto.OrderResponse;
+import io.github.mitohondriyaa.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderResponse placeOrder(@RequestBody OrderRequest orderRequest) {
+        return orderService.placeOrder(orderRequest);
+    }
 }
