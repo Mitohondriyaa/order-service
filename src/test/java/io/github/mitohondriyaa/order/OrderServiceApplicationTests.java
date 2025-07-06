dec443e0a72de7741d34b5260d1323b9695fd32d
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-06 12:29:34 +0300

    Add integration test for MySQL

--- src/test/java/io/github/mitohondriyaa/order/OrderServiceApplicationTests.java
+++ src/test/java/io/github/mitohondriyaa/order/OrderServiceApplicationTests.java
@@ -1,15 +1,51 @@
 package io.github.mitohondriyaa.order;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
+	@Container
+	@ServiceConnection
+	static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8");
+	@LocalServerPort
+	Integer port;
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 	}
 
+	@Test
+	void shouldPlaceOrder() {
+		String requestBody = """
+				{
+					"skuCode": "iPhone_15",
+					"price": 799,
+					"quantity": 1
+				}
+				""";
+
+		RestAssured.given()
+				.contentType(ContentType.JSON)
+				.body(requestBody)
+				.when()
+				.post("/api/order")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("orderNumber", Matchers.notNullValue())
+				.body("skuCode", Matchers.is("iPhone_15"))
+				.body("price", Matchers.is(799))
+				.body("quantity", Matchers.is(1));
+	}
 }
