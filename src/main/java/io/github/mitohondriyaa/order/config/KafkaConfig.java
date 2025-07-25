4ea5ee17988d19a9df1403be63e6832a5bd3df1a
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-25 13:42:23 +0300

    Add default topic bean

---
+++ src/main/java/io/github/mitohondriyaa/order/config/KafkaConfig.java
@@ -0,0 +1,13 @@
+package io.github.mitohondriyaa.order.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic orderPlacedTopic() {
+        return new NewTopic("order-placed", 3, (short) 2);
+    }
+}
