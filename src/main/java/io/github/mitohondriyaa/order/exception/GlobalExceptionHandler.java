bcfce10a8d8dee8b104831a17bc4503bfd8051c8
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-28 17:21:28 +0300

    Add global exception handling

---
+++ src/main/java/io/github/mitohondriyaa/order/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package io.github.mitohondriyaa.order.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<Info> handleOutOfStockExceptions(
+        OutOfStockException exception
+    ) {
+        Info info = new Info(exception.getMessage());
+
+        return new ResponseEntity<>(info, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Info> handleServiceUnavailableException(
+        ServiceUnavailableException exception
+    ) {
+        Info info = new Info(exception.getMessage());
+
+        return new ResponseEntity<>(info, HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Info> handleOtherExceptions(
+        Exception exception
+    ) {
+        Info info = new Info(exception.getMessage());
+
+        return new ResponseEntity<>(info, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/main/java/io/github/mitohondriyaa/order/exception/Info.java
@@ -0,0 +1,14 @@
+package io.github.mitohondriyaa.order.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Info {
+    private String message;
+}
