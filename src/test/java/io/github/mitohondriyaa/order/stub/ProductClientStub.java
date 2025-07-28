8b3f0ed26136f271fc07b80d13da2ad2931efde1
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-28 17:25:36 +0300

    Add stub for product client

---
+++ src/test/java/io/github/mitohondriyaa/order/stub/ProductClientStub.java
@@ -0,0 +1,13 @@
+package io.github.mitohondriyaa.order.stub;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class ProductClientStub {
+    public static void stubProductCall(String productId) {
+        stubFor(get(urlEqualTo("/api/inventory/price/" + productId))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+                .withBody("500")));
+    }
+}
